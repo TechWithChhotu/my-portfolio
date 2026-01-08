9c451ad6278bf18ca2db0e8afed17cf8487d13be
Chhotu Patel <chhotustudymail@gmail.com>
2026-01-08 18:17:20 +0530

    cors error solved

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://chhotu-patel2025.netlify.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "PATCH"],
     credentials: true,
   })
