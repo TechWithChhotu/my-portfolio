b3f4712e401fb6ad359bb99a20beeea9cd3c7d59
Chhotu Patel <chhotustudymail@gmail.com>
2026-01-09 16:08:10 +0530

    deployment error solved

--- server/app.js
+++ server/app.js
@@ -8,8 +8,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://chhotuportfolio2026.netlify.app",
-    // origin: "http://localhost:5173",
+    // origin: "https://chhotuportfolio2026.netlify.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "PATCH"],
     credentials: true,
   })
--- server/utils/mailer.utils.js
+++ server/utils/mailer.utils.js
@@ -1,10 +1,27 @@
-// utils/mailer.js
+// // utils/mailer.js
 import nodemailer from "nodemailer";
 
+// export const mailer = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER, // your gmail
+//     pass: process.env.EMAIL_PASS, // app password
+//   },
+// });
 export const mailer = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // 🔥 VERY IMPORTANT
   auth: {
-    user: process.env.EMAIL_USER, // your gmail
-    pass: process.env.EMAIL_PASS, // app password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
+});
+// ✅ VERIFY SMTP CONNECTION (YAHI LAGANA HAI)
+mailer.verify((err, success) => {
+  if (err) {
+    console.log("❌ MAIL VERIFY ERROR:", err);
+  } else {
+    console.log("✅ MAIL SERVER READY");
+  }
 });
